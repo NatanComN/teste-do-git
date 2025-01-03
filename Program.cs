d64d5700e17a879c266dca9ec3dacf61c5450216
Natan Araujo <natanaocontr4rio@gmail.com>
2025-01-03 13:33:40 -0300

    adicionei ao invés de alterar

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Sexta modificação modificação: 13:31");
 Console.WriteLine("Quinta modificação: 13:11");
+Console.WriteLine("sétima modificação: 13:11");
